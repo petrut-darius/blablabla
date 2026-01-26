0b11a2adbbcf134e1bc1627eaf31aee02d65858a
Petrut Darius-Ioan <131020605+petrut-darius@users.noreply.github.com>
2026-01-26 22:41:08 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -11,67 +11,96 @@ $crawler = new Client([
     ],
     'timeout' => 15,
 ]);
+
+libxml_use_internal_errors(true);
+
+/*
+subcategories
+$visitedLinks = [];
+$results = [];
+
+$row = 1;
+if(($file = fopen("../storage/csv/mainCategories.csv", "r")) !== FALSE) {
+    while(($data = fgetcsv($file, 51)) !== FALSE) {
+        //print_r($data[0] . PHP_EOL);
+        
+        
+        $subCategoryResponse = $crawler->get($data[0]);
+        $subCategoryHtml = (string) $subCategoryResponse->getBody();
+
+        $subCategoryDOM = new DOMDocument();
+        $subCategoryDOM->loadHTML($subCategoryHtml);
+        $subCategoryXpath = new DOMXPath($subCategoryDOM);
+    
+        //$outputFile = fopen("../storage/csv/subCategories.csv", "a+");
+        $subCategoryNodes = $subCategoryXpath->query("//div[contains(@class, 'col-xs-4')]/a[1]");
+    
+        foreach($subCategoryNodes as $subCategoryNode) {
+            $subCategoryLink = $subCategoryNode->getAttribute("href");
+
+            if(in_array($subCategoryLink, $visitedLinks)) {
+                continue;
+            }else{
+                $visitedLinks[] = $subCategoryLink;
+            }
+
+            if(!str_starts_with($subCategoryLink, "http")) {
+                $subCategoryLink = "https://www.bebebliss.ro" . $subCategoryLink;
+            }
+
+            $results[] = [
+                (string) $subCategoryLink
+            ];
+        }
+    }
+}
+fclose($file);
+
+$outputFile = fopen("../storage/csv/subCategories.csv", "w+");
+foreach($results as $result) {
+    fputcsv($outputFile, $result);
+}
+fclose($outputFile);
+*/
+
+
+/*
+//mainCategories
+
 $mainResponse = $crawler->get("https://www.bebebliss.ro/");
 $mainHtml = (string) $mainResponse->getBody();
 
-libxml_use_internal_errors(true);
 $doc = new DOMDocument();
 $doc->loadHTML($mainHtml);
 $xpath = new DOMXPath($doc);
 
 
+$visitedProducts = [];
 $results = [];
 
-$mainCategoryNodes = $xpath->query("//ul[contains(@class, 'side-menu')]//a");
+$mainCategoryNodes = $xpath->query("//li[contains(@class, 'menu-full-width')]/a[1]");
+$file = fopen("../storage/csv/mainCategories.csv", "w+");
 
 foreach($mainCategoryNodes as $mainCategoryNode) {
     $categoryLink = $mainCategoryNode->getAttribute("href");
     
+    if(in_array($categoryLink, $visitedProducts)) {
+        continue;
+    }else{
+        $visitedProducts[] = $categoryLink;
+    }
+
     if(!str_starts_with($categoryLink, "http")) {
         $categoryLink = "https://www.bebebliss.ro/" . $categoryLink;
     }
-    
-    $categoryResponse = $crawler->get($categoryLink);
-    $categoryHtml = (string) $categoryResponse->getBody();
 
-    $categoryDoc = new DOMDocument();
-    $categoryDoc->loadHTML($categoryHtml);
-    $categoryXpath = new DOMXPath($categoryDoc);
-
-    $categoryItemNodes = $categoryXpath->query("//ul[contains(@class, 'products-grid')]//li//a");
-
-    foreach($categoryItemNodes as $categoryItemNode) {
-        $itemLink = $categoryItemNode->getAttribute("href");
-
-        if(!str_starts_with($itemLink, "http")) {
-            $itemLink = "https://www.bebebliss.ro/" . $itemLink;
-        }
-
-        $itemResponse = $crawler->get($itemLink);
-        $itemHtml = (string) $itemResponse->getBody();
-
-        $itemDoc = new DOMDocument();
-        $itemDoc->loadHTML($itemHtml);
-        $itemXpath = new DOMXPath($itemDoc);
-
-        $itemTitle = trim($itemXpath->evaluate("string(//h1[contains(@class, 'product-name')])"));
-        $itemCode = trim($itemXpath->evaluate("string((//div[contains(@class, 'sku')]//span)[1])"));
-        $itemPrice = trim($itemXpath->evaluate("string(//span[contains(@class, 'special-price')])"));
-        $itemShortDescription = trim($itemXpath->evaluate("string(//div[contains(@class, 'std-short')])"));
-        $itemLongDescription = trim($itemXpath->evaluate("string(//div[contains(@class, 'std')])"));
-        $itemVideoSource = trim($itemXpath->evaluate("string(//a[contains(@class, 'ytp-impression-link')]/@href)"));
-
-        $results[] = [
-            "title" => $itemTitle,
-            "code" => $itemCode,
-            "price" => $itemPrice,
-            "short_description" => $itemShortDescription,
-            "long_description" => $itemLongDescription,
-            "video_source" => $itemVideoSource,
-        ];
-
-        sleep(1);
-    }
+    $results[] = [
+        (string) $categoryLink
+    ];
 }
 
-print_r($results[0]);
+foreach ($results as $result) {
+    fputcsv($file, $result);
+}
+fclose($file);
+*/
